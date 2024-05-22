124e59fe15f4beeec34fd2cb892ff50c87fd63da
Sunantha <142298455+Sunantha2411@users.noreply.github.com>
2024-05-22 19:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request,redirect
 import string
 import random
 app = Flask(__name__)
